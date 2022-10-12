99435ccf6ec83002c38371516aed6eecf1bbbf32
Reyesalex777 <Reyesalex777@gmail.com>
2022-10-12 14:57:50 -0700

    update funcitionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 /*----- constants -----*/
-const words = ['space', 'earth', 'planet', 'universe', 'solar', 'sun', 'moon','superNova', ]; // Word library!
+const words = ['space', 'earth', 'planet', 'universe', 'sun', 'moon','supernova', 'star' ]; // Word library!
 const MAX_WRONG = 5; // the amount body parts(6)
-
+// const IMGS = [
+// "IMG/SPACEMANIMG/spaceman-0.jpg",
+// "IMG/SPACEMANIMG/spaceman-1.jpg",
+// "IMG/SPACEMANIMG/spaceman-2.jpg",
+// "IMG/SPACEMANIMG/spaceman-3.jpg",
+// "IMG/SPACEMANIMG/spaceman-4.jpg",
+// "IMG/SPACEMANIMG/spaceman-5.jpg",
+// "IMG/SPACEMANIMG/spaceman-6.jpg"
+// ];
 
 
 /*----- state variables -----*/
@@ -15,11 +23,12 @@ let randWord; //generated random word
 
 
 /*----- stored elements  -----*/
+const imgEl = document.getElementById('spaceman');
 const playBtn = document.getElementById('newgamebtn');
-const messageEl = document.querySelector('message');
-const attempts = document.querySelector('lives');
+const messageEl = document.querySelector('.message');
+const attempts = document.querySelector('.lives');
 const letters = [...document.querySelectorAll('section > button')];
-const secretWord = document.querySelector('secretword');
+const secretWord = document.querySelector('.secretword');
 
 
 
@@ -36,7 +45,7 @@ playBtn.addEventListener('click', init);
 
   function init() {
     wrongGuesses = [];
-    const maxIdx = Math.floor(Math.random * words.length);
+    const maxIdx = Math.floor(Math.random() * words.length);
     console.log(guess);
     randWord = words[maxIdx].toUpperCase().split('')
     guess = randWord.map(ltr => ltr === ' ' ? ' ' : '_');
@@ -46,6 +55,7 @@ playBtn.addEventListener('click', init);
   
   function render() {
     renderMessage();
+    imgEl.src = `IMG/SPACEMANIMG/spaceman-${wrongGuesses.length}.jpg`;
     secretWord.textContent = guess.join('');
     renderBtn();
   }
@@ -54,7 +64,7 @@ playBtn.addEventListener('click', init);
     if (gameStatus === 'W') {
       messageEl.textContent = `You guessed right!`
     } else if (gameStatus === 'L') {
-      messageEl.textContent = `you ran out of guesses, the word was ${secretWord}, play again!`
+      messageEl.textContent = `you ran out of guesses, the word was ${randWord.join('')}, play again!`
     } else {
       messageEl.textContent = `${MAX_WRONG - wrongGuesses.length + 1} lives left`;
     }
@@ -76,23 +86,29 @@ playBtn.addEventListener('click', init);
   }
   
   function handleChoice(evt) {
-    const letters = evt.target.textContent
-    console.log(guess);
+    const ltr = evt.target.textContent
+    console.log(ltr);
     if (
       gameStatus ||
-      // Guards
-      !letters.includes(evt.target) ||
-      wrongGuesses.includes(letters) ||
-      guess.includes(letters)
+      // Guards in order to not return value, if not clicking letter button
+      // !ltr.includes(evt.target) || 
+      //better guard than ^
+      evt.target.tagName !== 'BUTTON' ||
+      //Guards in order to not return value if letter already selected is wrong
+      wrongGuesses.includes(ltr) ||
+      //Guards in order to not return value if letter already selected is correct
+      guess.includes(ltr)
     ) return;
   
-    if (secretWord.includes(letters)) {
-      //if guess is correct
-      secretWord.forEach(function(char, idx) {
-        if (char === letters) guess[idx] = ltr
+    //randWord is variable of the randWord picked
+    if (randWord.includes(ltr)) {
+      //if guess is correct. if (ltrs) is correctly picked
+      randWord.forEach(function(char, idx) {
+        if (char === ltr) guess[idx] = ltr
       }); 
       } else {
-        wrongGuesses.push(letters);
+        //if our guess(ltrs) is not part of our word
+        wrongGuesses.push(ltr);
       }
       gameStatus = getGameStatus();
       render();
